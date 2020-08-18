84c3ad0e28e1b135f705fb349554b25a62dfd45c
brumbrumpifpaf <69747323+brumbrumpifpaf@users.noreply.github.com>
2020-08-18 11:57:45 +0200

    Update uniqueCharacters.java

--- Strings/uniqueCharacters.java
+++ Strings/uniqueCharacters.java
@@ -1,1 +1,13 @@
-
+public static boolean allUnique (String word) {
+	HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+	char[] chars = word.toCharArray();
+	for (char c: chars) {
+		int check = map.getOrDefault(c, 0);
+		if (check == 0) {
+			map.put(c, 1);
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
