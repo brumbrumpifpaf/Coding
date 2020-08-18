cce27aecc642206c063ff02120e7270bc4578c48
brumbrumpifpaf <69747323+brumbrumpifpaf@users.noreply.github.com>
2020-08-18 11:52:51 +0200

    Update permutation.java

--- Strings/permutation.java
+++ Strings/permutation.java
@@ -1,13 +1,13 @@
 public static void permut (String word, HashMap<Character, Integer> map) {
-		char[] chars = word.toCharArray();
-		for (char w: chars) {
-			if (!map.containsKey(w)) {
-				map.put(w, 1);
-			} else {
-				map.put(w, map.get(w) + 1);
-			}
+	char[] chars = word.toCharArray();
+	for (char w: chars) {
+		if (!map.containsKey(w)) {
+			map.put(w, 1);
+		} else {
+			map.put(w, map.get(w) + 1);
 		}
-		System.out.println(map);
+	}
+	System.out.println(map);
 	}
 	
 public static void permut2 (String word, HashMap<Character, Integer> map) {
